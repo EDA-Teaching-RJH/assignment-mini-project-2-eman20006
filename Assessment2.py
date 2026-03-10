b8d5d3544b7904c50eb615de1eb4d9ae4c46c883
eman20006 <ef392@kent.ac.uk>
2026-03-10 12:38:39 +0000

    dictionaries

---
+++ Assessment2.py
@@ -0,0 +1,22 @@
+import contest
+
+def main():
+    def intro():
+        print("Hello everyone and welcome to")
+        print("...")
+        print("The Big Pub Quiz")
+        ha = contest.host["name"]
+        print("I am your host" + ha)
+
+
+
+
+
+
+
+
+
+
+
+
+    main()
---
+++ contest.py
@@ -0,0 +1,46 @@
+
+host - {
+    "name": "Tim Reads",     #ha
+    "age": 42,              #hb
+    "country": "England"    #hc
+}
+
+contestant1 - {
+    "firstname": "Bella",   #1a
+    "lastname": "Spooner",  #1b
+    "age": 24,              #1c
+    "country": "Germany",   #1d
+    "favesubj": "Food"      #1e etc for rest of dictionaries
+}
+
+contestant2 - {
+    "firstname": "Charlie",
+    "lastname": "Beer",
+    "age": 36,  
+    "country": "France",
+    "favesubj": "Sport"
+}
+
+contestant3 - {
+    "firstname": "Kian",
+    "lastname": "Franklin",
+    "age": 28,  
+    "country": "England",
+    "favesubj": "History"
+}
+
+contestant4 - {
+    "firstname": "Max",
+    "lastname": "Hilling",
+    "age": 31,  
+    "country": "Greece",
+    "favesubj": "Geography"
+}
+
+contestant5 - {
+    "firstname": "Jessy",
+    "lastname": "Jones",
+    "age": 25,  
+    "country": "Spain",
+    "favesubj": "Animals"
+}
