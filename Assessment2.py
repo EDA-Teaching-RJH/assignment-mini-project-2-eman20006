54611a80f429ac0c83053191fba579860580c95d
eman20006 <ef392@kent.ac.uk>
2026-03-10 13:37:26 +0000

    Made intro function

--- Assessment2.py
+++ Assessment2.py
@@ -1,22 +1,35 @@
 import contest
-
+pos = ["1", "2", "3", "4", "5"]
+cont = ["Bella", "Charlie", "Kian", "Max", "Jessy"]
 def main():
     def intro():
-        print("Hello everyone and welcome to")
-        print("...")
-        print("The Big Pub Quiz")
+        print("Hello everyone and welcome to\n...\nThe Big Pub Quiz!")
         ha = contest.host["name"]
-        print("I am your host" + ha)
+        print("I am your host: " + ha)
+        print("Here are your contestants for the night:")
+        print(pos[0], ": "+ cont[0])
+        print(pos[1], ": "+ cont[1])
+        print(pos[2], ": "+ cont[2])
+        print(pos[3], ": "+ cont[3])
+        print(pos[4], ": "+ cont[4])
+        y = input("And finally, our special guest for the evening: ")
+        pos.append("6")
+        cont.append(y)
+        sixe = input("How are we all feeling contestants? ")
+        xx = contest.cont1["feeling"]
+        xy = contest.cont2["feeling"]
+        xz = contest.cont3["feeling"]
+        ff = contest.cont4["feeling"]
+        rt = contest.cont5["feeling"]
 
+        print(cont[0],":" + xx) 
+        print(cont[1],":" + xy) 
+        print(cont[2],":" + xz) 
+        print(cont[2],":" + ff) 
+        print(cont[4],":" + rt) 
+        print(cont[5],":" + sixe)
+        print("Wonderful, let's get on with the games!")
+    intro()
 
-
-
-
-
-
-
-
-
-
-
-    main()
+    
+main()
--- contest.py
+++ contest.py
@@ -1,46 +1,59 @@
 
-host - {
+host = {
     "name": "Tim Reads",     #ha
     "age": 42,              #hb
     "country": "England"    #hc
 }
 
-contestant1 - {
-    "firstname": "Bella",   #1a
-    "lastname": "Spooner",  #1b
-    "age": 24,              #1c
-    "country": "Germany",   #1d
+cont1 = {
+    "name": "Bella Spooner",   #1a
+    "age": 24,              #1b
+    "country": "Germany",   #1c
+    "feeling": "Confident",  #1d
     "favesubj": "Food"      #1e etc for rest of dictionaries
 }
 
-contestant2 - {
-    "firstname": "Charlie",
-    "lastname": "Beer",
+cont2 = {
+    "name": "Charlie Beer",
     "age": 36,  
     "country": "France",
+    "feeling": "A little nervous",
     "favesubj": "Sport"
 }
 
-contestant3 - {
-    "firstname": "Kian",
-    "lastname": "Franklin",
+cont3 = {
+    "name": "Kian Franklin",
     "age": 28,  
     "country": "England",
+    "feeling": "Good thank you",
     "favesubj": "History"
 }
 
-contestant4 - {
-    "firstname": "Max",
-    "lastname": "Hilling",
+cont4 = {
+    "name": "Max Hilling",
     "age": 31,  
     "country": "Greece",
-    "favesubj": "Geography"
+    "feeling": "Ready-ish",
+    "favesubj": "Pop Culture"
 }
 
-contestant5 - {
-    "firstname": "Jessy",
-    "lastname": "Jones",
+cont5 = {
+    "name": "Jessy Jones",
     "age": 25,  
     "country": "Spain",
+    "feeling": "Good",
     "favesubj": "Animals"
+}
+
+Topics = {
+    "1": "Science",
+    "2": "Maths",
+    "3": "Pop Culture",
+    "4": "Mystery",
+    "5": "General Knowledge",
+    "6": "Food",
+    "7": "Animals",
+    "8": "Music",
+    "9": "History",
+    "10": "Sport"
 }
