49c212688ef91480ee1fda5da1940862d5a603cf
eman20006 <ef392@kent.ac.uk>
2026-03-16 05:17:37 +0000

    began noting down my code for refference

--- Assessment2.py
+++ Assessment2.py
@@ -1,20 +1,20 @@
-import contest
-import re    
-import random
-import csv
-pos = ["1", "2", "3", "4", "5"]
+import contest #this imports my custom library
+import re      #import allows for support working with regex
+import random   # allows me to make random number generators for random events
+import csv          # allows me to implement classes and work in csv format
+pos = ["1", "2", "3", "4", "5"]   #here are my 2 lists, outside my main function so it is convenient to call and use them
 cont = ["Bella", "Charlie", "Kian", "Max", "Zak"]
-ha = contest.host["name"]
+ha = contest.host["name"]  #accessing data from dictionary from custom import
 def main():
     print("Hello everyone and welcome to ...\nThe Big Pub Quiz!")
     
     print(ha + ": I am your host " + ha)
 
-    def pick22():
+    def pick22():   #a function that gets called later, allows player to continue or end game
         pick2 = input(ha + ": Are you ready for the categories? ")
         if pick2 == "yes":
                 print(ha + ": Wonderful, let's get on with the games!")
-                topics()
+                topics() #function is later in code
         elif pick2 == "no":
                 print(ha + ": GAME OVER")
                 print("SHUTTING DOWN ...")
@@ -22,77 +22,78 @@ def main():
         else:   
                 print(ha + ": Invalid input")
                 pick22()
-    def age():
+    def age(): #this function also gets called later
         x = (input("Age: "))
-        xx = re.sub(r'[^0-9 ]', '', x)
-        print(ha + ": Confirmed age is " + xx)
-        contest.cont6.update({"age": xx})
-           
-    def intro():
-        
+        xx = re.sub(r'[^0-9 ]', '', x) #regex used to ensure only numbers are recieved
+        #any added characters such as letters or symbols are discarded
+        if xx == "": #used empty apostrophe as if only letters or symbols input, stored input would be blank
+            #made it so i can have user retry input
+            print(ha + ": Invalid input")
+            age()
+            
+        else:
+            print(ha + ": Confirmed age is " + xx)
+            contest.cont6.update({"age": xx}) #when input confirmed as only number, updates data in dictionary
+            
+    def intro(): #also function that gets called later, allows player to continue or end game
+                 #additionally, has the intro to the game with interactive start-up
         pick1 = input(ha + ": Would you like to continue? ")
 
-        if pick1 == "yes":
+        if pick1 == "yes": #runs code if player chooses to continue
 
             print(ha + ": Here are your contestants for the night:")
-            aa = contest.cont1["age"]
-            bb = contest.cont2["age"]
-            cc = contest.cont3["age"]
-            dd = contest.cont4["age"]
-            ee = contest.cont5["age"]
-            print(pos[0], ": "+ cont[0] + " " + str(aa))
-            print(pos[1], ": "+ cont[1] + " " + str(bb))
-            print(pos[2], ": "+ cont[2] + " " + str(cc))
-            print(pos[3], ": "+ cont[3] + " " + str(dd))
-            print(pos[4], ": "+ cont[4] + " " + str(ee))
+            #previusly i accessed dictionary data and made it equal to a variable to then add to a print function
+            #cut out middle-man and added dictinary data access code straight in the print function
+            print(pos[0], ": "+ cont[0] + " " + str(contest.cont1["age"])) #calls both list positions and dictionary data from custom import
+            print(pos[1], ": "+ cont[1] + " " + str(contest.cont2["age"]))
+            print(pos[2], ": "+ cont[2] + " " + str(contest.cont3["age"]))
+            print(pos[3], ": "+ cont[3] + " " + str(contest.cont4["age"]))
+            print(pos[4], ": "+ cont[4] + " " + str(contest.cont5["age"]))
             print(ha + ": And finally, our special guest for the evening: ")
             y = input("Name: ").capitalize() #make sure input has no added spaces or error adds
-            yy = re.sub(r'[^a-zA-Z ]', '', y).capitalize()
-            def hello(to="world"):
-                print(ha + ": Hello ", to)
+            yy = re.sub(r'[^a-zA-Z ]', '', y).capitalize() #regex, makes input limited to letters and captilizes first word
+                                                           #gets rid of symbols and numbers
+            def hello(to="world"):   #example of testing without just printing something, using a def function
+                print(ha + ": Hello ", to)   #this will greet player after inputting chosen name
             if __name__ == "__intro__":
                 intro()
 
-            hello(yy)
+            hello(yy) #calls the greeting def function
 
-            age()
-            z = input("Country: ") #same as for name
-            zz = re.sub(r'[^a-zA-Z ]', '', z).capitalize()
+            age()  #calls age function from earlier
+            z = input("Country: ") 
+            zz = re.sub(r'[^a-zA-Z ]', '', z).capitalize() #regex similar written code as earlier
             print(ha + ": Confirmed country is " + zz)
-            contest.cont6.update({"name": yy})
-            contest.cont6.update({"country": zz})
+            contest.cont6.update({"name": yy})  #updates data in specific dictionary, stores new data
+            contest.cont6.update({"country": zz}) #updates data in specific dictionary, stores new data
             
-            pos.append("6")
-            cont.append(yy)
-            cont6 = input(ha + ": How are we all feeling contestants? ")
-            cont66 = re.sub(r'[^a-zA-Z ]', '', cont6).capitalize()
-            contest.cont6.update({"feeling": cont66})
-            a = contest.cont1["feeling"]
-            b = contest.cont2["feeling"]
-            c = contest.cont3["feeling"]
-            d = contest.cont4["feeling"]
-            e = contest.cont5["feeling"]
-            
-            print(cont[0],": " + a) 
-            print(cont[1],": " + b) 
-            print(cont[2],": " + c) 
-            print(cont[2],": " + d) 
-            print(cont[4],": " + e) 
-            print(cont[5],": " + cont66)
+            pos.append("6") #adds to position number list
+            cont.append(yy) # adds to contestant name list, now parallel with number above
+            cont6 = input(ha + ": How are we all feeling contestants? ") #prompts plaer input
+            cont66 = re.sub(r'[^a-zA-Z ]', '', cont6).capitalize() #regex aims to remove input errors
+            contest.cont6.update({"feeling": cont66}) #updates data in specific dictionary, stores new data
+            #previusly i accessed dictionary data and made it equal to a variable to then add to a print function
+            #cut out middle-man and added dictinary data access code straight in the print function
+            print(cont[0],": " + contest.cont1["feeling"]) 
+            print(cont[1],": " + contest.cont2["feeling"]) 
+            print(cont[2],": " + contest.cont3["feeling"]) 
+            print(cont[2],": " + contest.cont4["feeling"]) 
+            print(cont[4],": " + contest.cont5["feeling"]) 
+            print(cont[5],": " + contest.cont6["feeling"])
            
-            pick22()
+            pick22() #calls function 
                     
-        elif pick1 == "no":
+        elif pick1 == "no": #prints and ends code
             print(ha + ": GAME OVER")
             print("SHUTTING DOWN ...")
             
 
-        else:
+        else: #if neither "yes" or "no" are input, makes user try again
             print(ha + ": Invalid input")
-            intro()
+            intro() #calls back to start of def function where choice to continue or end
 
-    def science():
-        print(ha + ": You have chosen " + contest.topics["1"])
+    def science(): #currently empty functions soon to be filled with questions and answers, not sure how I will display this tho
+        print(ha + ": You have chosen " + contest.topics["1"]) # considering I am grabbing dictionaries, probs should add to said dictionaries
         
     def maths():
         print(ha + ": You have chosen " + contest.topics["2"])
@@ -122,7 +123,7 @@ def main():
         print(ha + ": You have chosen " + contest.topics["10"])
     
 
-    def topics():
+    def topics(): #produces lis of categories which players can choose
         print(ha + ": Here are your categories:")
         print("1." + contest.topics["1"])
         print("2." + contest.topics["2"])
@@ -134,37 +135,28 @@ def main():
         print("8." + contest.topics["8"])
         print("9." + contest.topics["9"])
         print("10." + contest.topics["10"])
-        randcont()
+        randcont() #calls function
     def randcont():
-        numb1 = random.randrange(1, 6)
+        numb1 = random.randrange(1, 6) #picks a random numbe btween 1-6 representing contestants
         print(ha + ": We will start with contestant number "  + str(numb1))
-        if numb1 == 1:
+        if numb1 == 1 or numb1 == 2 or numb1 == 3 or numb1 == 4 or numb1 == 5: #formatting like this 
+            #meant less repeated lines of similar code
             print(ha + ": Which topic would you like contestant " + str(numb1) + "? ")
-            choice1_5()
-        if numb1 == 2:
-            print(ha + ": Which topic would you like contestant " + str(numb1) + "? ")
-            choice1_5()
-        if numb1 == 3:
-            print(ha + ": Which topic would you like contestant " + str(numb1) + "? ")
-            choice1_5()
-        if numb1 == 4:
-            print(ha + ": Which topic would you like contestant " + str(numb1) + "? ")
-            choice1_5()
-        if numb1 == 5:
-            print(ha + ": Which topic would you like contestant " + str(numb1) + "? ")
-            choice1_5()
-        if numb1 == 6:
-            choice6()
+            choice1_5() #as 1-5 arent users, I call this function
+        
+        if numb1 == 6: #this is user number
+            choice6() #calls this function
         else:
             print(ha + ": Technical difficulty")
             
 
     def choice1_5():
-        number = random.randint(1, 10)
+        number = random.randint(1, 10) # random number generator to pick 1 of the 10 topics at random
+        #so the game is never completely the same each time
         print(ha + ": Contestant has chosen: " + str(number))
         
         if number == 1:
-            science()
+            science() #each one of these calls a specific topic function
            
         elif number == 2:
             maths()
@@ -199,7 +191,8 @@ def main():
             
     def choice6():
         numbr = input(ha + ": Which topic would you like contestant 6? ")
-        print(ha + ": You chose: " + str(numbr))
+        print(ha + ": You chose: " + str(numbr)) #asks user to choose a topic and depending on input
+        #depends on which category
         if numbr == 1:
             science()
 
